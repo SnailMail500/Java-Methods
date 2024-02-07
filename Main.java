7a5fb5064a05ce5f665684005cb841c913c37975
Alex <83418697+SnailMail500@users.noreply.github.com>
2024-02-07 10:10:55 +0000

    Added files and examples of a basic method with a return too

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    static void newMethod(){//static means that the method belongs to class Main and is not an object of that class
+        //void means that nothing will be returned
+        //Basically a subroutine (or a function if it returns something)
+        System.out.println("This method has been executed if this statement prints");
+    }
+    static String returnString(String input){//the function takes the parameter and changes its identifier temporarily to "input"
+        return input;//then immediately sends it back
+    }
+    public static void main (String[] args){
+        newMethod();
+        String name = "bob";
+        System.out.println(returnString(name));//about as simple a method involving return as possible- takes name... passes it straight back to be printed.
+        //name is passed through as a parameter
+    }
+}
